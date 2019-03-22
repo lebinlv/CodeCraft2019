f4ca1dbca3e605188aee3dd8fd3b8c705de43be6
KOMOhub <1035844563@qq.com>
2019-03-22 13:46:18 +0800

    dispatch & Capacity

--- CodeCraft-2019/CodeCraft-2019.cpp
+++ CodeCraft-2019/CodeCraft-2019.cpp
@@ -1,11 +1,10 @@
-
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <string>
 #include <queue>
 #include <algorithm>
-#include <limits>   // std::numeric_limits
-#include <string>
+
 #include <chrono>   // speed test
 
 #include "object.hpp"
@@ -15,129 +14,179 @@
 #define ROAD_VECTOR_RESERVE_SIZE   150 
 #define USE_CXX_SSTREAM 0
 
+
 using namespace std;
 
+static int global_time = 0;//q全局时间
 
 int main(int argc, char *argv[])
 {
-/* SDK code */
-    cout << "Begin" << endl;
+	/* SDK code */
+	cout << "Begin" << endl;
 
-	if(argc < 5){
-        cout << "please input args: carPath, roadPath, crossPath, answerPath" << endl;
-        exit(1);
-    }
+	if (argc < 5) {
+		cout << "please input args: carPath, roadPath, crossPath, answerPath" << endl;
+		exit(1);
+	}
 
-    string carPath(argv[1]);
-    string roadPath(argv[2]);
-    string crossPath(argv[3]);
-    string answerPath(argv[4]);
-    cout << "carPath is " << carPath << endl;
-    cout << "roadPath is " << roadPath << endl;
-    cout << "crossPath is " << crossPath << endl;
-    cout << "answerPath is " << answerPath << endl;
-/* END of SDK code */
+	string carPath(argv[1]);
+	string roadPath(argv[2]);
+	string crossPath(argv[3]);
+	string answerPath(argv[4]);
 
-
-/* Read configuration from file，考虑到文件体积较小，使用 fgets 函数（大文件可使用 sstream + getline）*/
-    FILE *fptr;
-    char line_buffer[MAXIMUM_LENGTH_PER_LINE];
-
-  /* Read car configuration */
-    //priority_queue<CAR *, vector<CAR *>, CAR::Compare> car_pri_queue; // 建立 priority queue 将所有车辆信息保存
-    vector<CAR*> car_vec;
-    car_vec.reserve(CAR_VECTOR_RESERVE_SIZE);
-
-    fptr = fopen(carPath.c_str(), "r");
-    if (fptr == NULL) {
-        cout << "can't open car configuration file: " << carPath << endl;
-        exit(-1);
-    }
-
-    //auto start = std::chrono::steady_clock::now();
-    /* 逐行读取车辆信息 */
-    fgets(line_buffer, 50, fptr); // 似乎只有文件第一行为注释......
-    while (fgets(line_buffer, 50, fptr)) {
-        //if(line_buffer[0] == '(') {
-            auto pCar = new CAR;
-            sscanf(line_buffer, "(%d, %d, %d, %d, %d)",
-                   &(pCar->id), &(pCar->from), &(pCar->to), &(pCar->speed), &(pCar->plan_time));
-            //car_pri_queue.push(pCar);
-            car_vec.push_back(pCar);
-        //}
-    }
-    fclose(fptr);
-
-    /* 如果两辆车的计划时间相等，则先速度快的车优先级高；否则先出发的车优先级高。 */
-    sort(car_vec.begin(), car_vec.end(),
-         [](CAR* a, CAR* b) -> bool { return 
-         (a->plan_time ==  b->plan_time) ? (a->speed > b->speed) : (a->plan_time < b->plan_time);});
-    // auto end = std::chrono::steady_clock::now();
-    // std::chrono::duration<double, std::micro> elapsed = end - start; // std::micro 表示以微秒为时间单位
-    // std::cout << "time: " << elapsed.count() << "us" << std::endl;
-
-  /* End of read car configuration */
+	cout << "carPath is " << carPath << endl;
+	cout << "roadPath is " << roadPath << endl;
+	cout << "crossPath is " << crossPath << endl;
+	cout << "answerPath is " << answerPath << endl;
+	/* END of SDK code */
 
 
+	/* Read configuration from file，考虑到文件体积较小，使用 fgets 函数（大文件可使用 sstream + getline）*/
+	FILE *fptr;
+	char line_buffer[MAXIMUM_LENGTH_PER_LINE];
 
-  /* Read cross configuration, mainly to get the number of cross */
-    fptr = fopen(crossPath.c_str(), "r");
-    if (fptr == NULL) {
-        cout << "can't open cross configuration file: " << roadPath << endl;
-        exit(-1);
-    }
-    int cross_count = 0;
-    /* 逐行读取信息 */
-    fgets(line_buffer, 50, fptr); // 似乎只有文件第一行为注释......
-    while (fgets(line_buffer, 50, fptr)) {
-        //if(line_buffer[0] == '(') {
-            cross_count ++;
-        //}
-    }
-    fclose(fptr);
-  /* End of Read cross configuration */
+	/* Read car configuration */
+	//priority_queue<CAR *, vector<CAR *>, CAR::Compare> car_pri_queue; // 建立 priority queue 将所有车辆信息保存
+	vector<CAR*> car_vec;
+	car_vec.reserve(CAR_VECTOR_RESERVE_SIZE);
+
+	fptr = fopen(carPath.c_str(), "r");
+	if (fptr == NULL) {
+		cout << "can't open car configuration file: " << carPath << endl;
+		exit(-1);
+	}
+
+	//auto start = std::chrono::steady_clock::now();
+	/* 逐行读取车辆信息 */
+	fgets(line_buffer, 50, fptr); // 似乎只有文件第一行为注释......
+	while (fgets(line_buffer, 50, fptr)) {
+		//if(line_buffer[0] == '(') {
+		auto pCar = new CAR;
+		sscanf(line_buffer, "(%d, %d, %d, %d, %d)",
+			&(pCar->id), &(pCar->from), &(pCar->to), &(pCar->speed), &(pCar->plan_time));
+		//car_pri_queue.push(pCar);
+		car_vec.push_back(pCar);
+		//}
+	}
+	fclose(fptr);
+	sort(car_vec.begin(), car_vec.end(),
+		[](CAR* a, CAR* b) -> bool { return
+		(a->plan_time == b->plan_time) ? (a->speed > b->speed) : (a->plan_time < b->plan_time); });
+
+	// auto end = std::chrono::steady_clock::now();
+	// std::chrono::duration<double, std::micro> elapsed = end - start; // std::micro 表示以微秒为时间单位
+	// std::cout << "time: " << elapsed.count() << "us" << std::endl;
+
+	/* End of read car configuration */
 
 
 
-  /* Read road configuration, and build GRAPH at the same time */
-    vector<ROAD*> road_vec;
-    road_vec.reserve(ROAD_VECTOR_RESERVE_SIZE);
-
-    fptr = fopen(roadPath.c_str(), "r");
-    if (fptr == NULL) {
-        cout << "can't open road configuration file: " << roadPath << endl;
-        exit(-1);
-    }
-
-    CROSS::id_type from, to;
-    bool isDuplex;
-
-	//(5000, 15, 6, 2, 1, 2, 1)
-    /* 逐行读取道路信息 */
-    fgets(line_buffer, 50, fptr); // 似乎只有文件第一行为注释......
-    while (fgets(line_buffer, 50, fptr)) {
-        //if(line_buffer[0] == '(') {
-            auto pRoad = new ROAD;
-            sscanf(line_buffer, "(%d, %d, %d, %d, %d, %d, %d)",
-                &(pRoad->id), &(pRoad->length), &(pRoad->max_speed), &(pRoad->channel), 
-                &from, &to, &isDuplex);
-            road_vec.push_back(pRoad);
-        //}
-    }
-    fclose(fptr);
-
-  /*End of read road configuration */
-
-/* END of read configuration from file */
+	/* Read cross configuration, mainly to get the number of cross */
+	// fptr = fopen(crossPath.c_str(), "r");
+	// if (fptr == NULL) {
+	//     cout << "can't open cross configuration file: " << roadPath << endl;
+	//     exit(-1);
+	// }
+	// int cross_count = 0;
+	// /* 逐行读取信息 */
+	// fgets(line_buffer, 50, fptr); // 似乎只有文件第一行为注释......
+	// while (fgets(line_buffer, 50, fptr)) {
+	//     //if(line_buffer[0] == '(') {
+	//         cross_count ++;
+	//     //}
+	// }
+	// fclose(fptr);
+	/* End of Read cross configuration */
 
 
-    /* TODO:process */
 
-    /* End of process */
+	/* Read road configuration, and build GRAPH at the same time */
+	//vector<ROAD*> road_vec;
+	//road_vec.reserve(ROAD_VECTOR_RESERVE_SIZE);
+	map<ROAD::id_type, ROAD*> road_map;
 
-    /* TODO:write output file */
+	GRAPH graph;
 
-    /* End of write out file */
+	int test_road_id, length, max_speed, channel, from, to, isDuplex;
 
-    return 0;
+	fptr = fopen(roadPath.c_str(), "r");
+	if (fptr == NULL) {
+		cout << "can't open road configuration file: " << roadPath << endl;
+		exit(-1);
+	}
+
+	/* 逐行读取道路信息 */
+	fgets(line_buffer, 50, fptr); // 似乎只有文件第一行为注释......
+	while (fgets(line_buffer, 50, fptr)) {
+		//if(line_buffer[0] == '(') {
+		sscanf(line_buffer, "(%d, %d, %d, %d, %d, %d, %d)", &test_road_id, &length, &max_speed, &channel, &from, &to, &isDuplex);
+		auto pRoad = new ROAD(test_road_id, length, max_speed, channel);
+		graph.add_node(test_road_id, from, to, pRoad->capacity, isDuplex);
+		//road_vec.push_back(pRoad);
+		road_map[test_road_id] = pRoad;
+		//}
+	}
+	fclose(fptr);
+
+	/*End of read road configuration */
+
+	/* END of read configuration from file */
+
+
+	/* TODO:process */
+	vector<CAR*> cars_running;//已经在运行的车辆
+
+	while (true) {
+		/*
+		车辆按照出发时间、行驶速度依次排序
+		*/
+		if (car_vec.begin == car_vec)
+			break;
+		/*sort(car_vec.begin(), car_vec.end(),
+		[](CAR* a, CAR* b) -> bool { return a->speed > b->speed; });
+		stable_sort(car_vec.begin(), car_vec.end(),
+		[](CAR* a, CAR* b) -> bool { return a->plan_time < b->speed; });*/
+		global_time++;
+		//取前面一百个
+		int element_idx;
+		if (car_vec.size() >= BATCH_SIZE)
+			for (element_idx = BATCH_SIZE - 1; element_idx--; element_idx >= 0)
+			{
+				if ((*(car_vec.begin() + element_idx))->plan_time <= global_time)
+					break;
+			}
+		else
+			element_idx = car_vec.size() - 1;
+
+		//释放行驶车辆的占用容量以及删除理论到达车辆
+		release_capacity(cars_running, global_time, &graph);
+
+		//调度车辆
+		for (vector<CAR*>::iterator car = car_vec.begin(); car != car_vec.begin() + element_idx + 1; car++)
+		{
+			//需要在内部解决开销容量减少问题，以及记录理论到达各个node的时间
+			graph.get_least_cost_route((*car)->from, (*car)->to, (*car)->speed, (*car), global_time);
+		}
+
+		//加入车辆inser
+		cars_running.insert(cars_running.end(), car_vec.begin(), car_vec.begin() + element_idx + 1);
+
+		//删除已经调度车辆
+		for (vector<CAR*>::iterator car = car_vec.begin(); car != car_vec.begin() + element_idx + 1; car++)
+		{
+			delete (*car);
+		}
+		car_vec.erase(car_vec.begin, car_vec.begin() + element_idx + 1);
+
+
+
+
+		/* End of process */
+
+
+	}
+	/* TODO:write output file */
+
+	/* End of write out file */
+	return 0;
 }
--- CodeCraft-2019/object.hpp
+++ CodeCraft-2019/object.hpp
@@ -3,155 +3,180 @@
 
 #include <cstdint>
 #include <vector>      // 寻路函数返回vector
-#include <queue>       // 寻路算法中使用 priority_queue
 #include <map>         // 权重字典
+#include <stack>
+//--------------------------
+#define BATCH_SIZE 100  //每个时间段发车数量
+#define CAPACITY_FACTOR 0.5//容量因子
+#define COST_FACTOR 1.1 //开销因子
+//-----------------------
 
 struct CROSS
 {
-    typedef uint8_t     id_type;  // 路口id类型
+	typedef uint8_t     id_type;  // 路口id类型
 };
 
 struct ROAD
 {
-    typedef uint16_t    id_type;
-    typedef uint8_t     length_type;
-    typedef uint8_t     speed_type;
-    typedef uint8_t     channel_type;
-    typedef int16_t     capacity_type;
+	typedef uint16_t    id_type;
+	typedef uint8_t     length_type;
+	typedef uint8_t     speed_type;
+	typedef uint8_t     channel_type;
+	typedef int16_t     capacity_type;
 
-    // (id,length,speed,channel,from,to,isDuplex)
-    id_type             id;
-    length_type         length;
-    speed_type          max_speed;
-    capacity_type       capacity;
-    channel_type        channel;
+	// (id,length,speed,channel,from,to,isDuplex)
+	id_type             id;
+	length_type         length;
+	speed_type          max_speed;
+	capacity_type       capacity;
+	channel_type        channel;
 
-    ROAD(){}
-    ROAD(id_type _id, length_type _length, speed_type _speed, channel_type _channel):
-         id(_id), length(_length), max_speed(_speed), channel(_channel) {
-        capacity = _length * _channel;
-    }
-    ~ROAD(){}
+	ROAD() {}
+	ROAD(id_type _id, length_type _length, speed_type _speed, channel_type _channel) :
+		id(_id), length(_length), max_speed(_speed), channel(_channel) {
+		capacity = _length * _channel;
+	}
+	~ROAD() {}
 };
 
 
 struct CAR
 {
-    typedef uint16_t    id_type;
-    typedef uint8_t     speed_type;
-    typedef uint8_t     time_type;
+	typedef uint16_t    id_type;
+	typedef uint8_t     speed_type;
+	typedef uint8_t     time_type;
 
-    // (id, from, to, speed, planTime)
-    id_type             id;
-    CROSS::id_type      from;
-    CROSS::id_type      to;
-    speed_type          speed;
-    time_type           plan_time;
+	// (id, from, to, speed, planTime)
+	id_type             id;
+	CROSS::id_type      from;
+	CROSS::id_type      to;
+	speed_type          speed;
+	time_type           plan_time;
 
-    CAR() {}
-    CAR(id_type _id, CROSS::id_type _from, CROSS::id_type _to, speed_type _speed, time_type _time):
-        id(_id), from(_from), to(_to), speed(_speed), plan_time(_time) {}
-    ~CAR() {}
+	//----------------
+	//记录节点
+	struct Past_node {
+		GRAPH::Node* node;
+		int arrive_time;
+		Past_node() {}
+		Past_node(GRAPH::Node* _node, int _arrive_time) :node(_node), arrive_time(_arrive_time) {}
+	};
+	stack<Past_node>		past_nodes;
+	//-----------------
 
-    /** 
-     * @brief 自定义优先队列的比较方法
-     * 
-     * 如果两辆车的计划时间相等，则先速度快的车优先级高；
-     * 否则先出发的车优先级高。
-     */
-    struct Compare {
-        bool operator()(const CAR* a, const CAR* b) {
-            return (a->plan_time == b->plan_time) ?
-                   (a->speed < b->speed) : (a->plan_time > b->plan_time);
-        }
-        bool operator()(const CAR & a, const CAR & b) {
-            return (a.plan_time == b.plan_time) ?
-                   (a.speed < b.speed) : (a.plan_time > b.plan_time);
-        }
-    };
-}; 
+	CAR() {}
+	CAR(id_type _id, CROSS::id_type _from, CROSS::id_type _to, speed_type _speed, time_type _time) :
+		id(_id), from(_from), to(_to), speed(_speed), plan_time(_time) {}
+	~CAR() {}
+
+	/**
+	* @brief 自定义优先队列的比较方法
+	*
+	* 如果两辆车的计划时间相等，则先速度快的车优先级高；
+	* 否则先出发的车优先级高。
+	*/
+	struct Compare {
+		bool operator()(const CAR* a, const CAR* b) {
+			return (a->plan_time == b->plan_time) ?
+				(a->speed < b->speed) : (a->plan_time > b->plan_time);
+		}
+		bool operator()(const CAR & a, const CAR & b) {
+			return (a.plan_time == b.plan_time) ?
+				(a.speed < b.speed) : (a.plan_time > b.plan_time);
+		}
+	};
+};
 
 
 
 class GRAPH
 {
-  public:
-    typedef double                    weight_type;  // 边的权重的数据类型
-    typedef uint16_t                  idx_type;     // 下标类型
-    typedef std::vector<CROSS::id_type>    route_type;   // 寻最短路函数的返回数据类型
-
-    struct Node{
-        CROSS::id_type       cross_id;        // 本节点代表的路口的id
-        ROAD::id_type        road_id;         // 到达本节点的路的id
-        //weight_type          weight;          // 到达本节点的路的权重
-        idx_type             weight_idx;      // 到达该节点的边的权重在权重数组中的下标
-
-        Node* next_node = nullptr;
-
-        Node(CROSS::id_type _cross_id, ROAD::id_type _road_id) : 
-             cross_id(_cross_id), road_id(_road_id) {weight_idx = node_count++;}
-        //Node(){}
-        ~Node(){delete next_node;}
-
-      private:
-        static idx_type      node_count;      // 静态变量用于统计节点个数，初始值为0
-    };
-
-    GRAPH(){}
-    ~GRAPH(){}
-
-    /**
-     * @brief 根据道路信息添加节点
-     * 
-     * @param road_id  道路id
-     * @param from     道路起点路口id
-     * @param to       道路终点路口id
-     * @param isDuplex 是否双向
-     */
-    void add_node(ROAD::id_type road_id, CROSS::id_type from, CROSS::id_type to, bool isDuplex);
-
-    /**
-     * @brief 根据车速计算新的权重数组
-     * 
-     * @param speed 车速
-     * @attention   必须在添加所有节点之后调用该函数
-     */
-    void add_weight_accord_to_speed(CAR::speed_type speed);
-
-    /**
-     * @brief Get the least cost route object
-     * 
-     * @param from 
-     * @param to 
-     * @param speed 
-     * @return route_type* 
-     */
-    route_type* get_least_cost_route(CROSS::id_type from, CROSS::id_type to, CAR::speed_type speed);
+public:
+	typedef double                    weight_type;  // 边的权重的数据类型
+	typedef uint16_t                  idx_type;     // 下标的数据类型,根据边的数目确定
+	typedef std::vector<CROSS::id_type>    route_type;   // 寻最短路函数的返回数据类型
+	std::vector<ROAD::capacity_type>                capacity_vec;
+	struct Node {
+		CROSS::id_type       cross_id;        // 本节点代表的路口的id
+		ROAD::id_type        road_id;         // 到达本节点的路的id
+											  //weight_type          weight;          // 到达本节点的路的权重
+		idx_type             weight_idx;      // 到达该节点的边的权重 在权重数组中的下标
+		idx_type             capacity_idx;    // 到达该节点的边的容量 在容量数组中的下标
 
 
-  private:
-    std::map<CAR::speed_type, weight_type*>  weight_map;  // 边对于不同速度的车，具有不同的权重
-    std::map<CROSS::id_type, Node*>          graph_map;   //
+		Node(CROSS::id_type _cross_id, ROAD::id_type _road_id) : cross_id(_cross_id), road_id(_road_id) {
+			weight_idx = capacity_idx = node_count++;
+		}
+		~Node() {}
+
+	private:
+		static idx_type      node_count;      // 静态变量用于统计节点个数，初始值为0
+	};
+
+	GRAPH() {}
+	~GRAPH() {}
+
+	/**
+	* @brief 根据道路信息添加节点
+	*
+	* @param road_id  道路id
+	* @param from     道路起点路口id
+	* @param to       道路终点路口id
+	* @param isDuplex 是否双向
+	*
+	* @attention 此函数并不检查 road_id 是否重复
+	*/
+	void add_node(ROAD::id_type road_id, CROSS::id_type from, CROSS::id_type to,
+		ROAD::capacity_type capacity, bool isDuplex);
+
+	/**
+	* @brief 根据车速计算新的权重数组
+	*
+	* @param speed 车速
+	* @attention   必须在添加所有节点之后调用该函数
+	*/
+	void add_weight_accord_to_speed(CAR::speed_type speed);
+
+	/**
+	* @brief Get the least cost route object
+	*
+	* @param from
+	* @param to
+	* @param speed
+	* @return route_type*
+	*/
+	route_type & get_least_cost_route(CROSS::id_type from, CROSS::id_type to, CAR::speed_type speed, CAR *car, int global_time);
 
 
-    weight_type*                        p_weight;    // 每次计算最短路径之前，根据车速重定向该指针
+private:
+	std::map<CAR::speed_type, weight_type*>         weight_map;  // 边对于不同速度的车，具有不同的权重
+	std::map<CROSS::id_type, std::vector<Node*> >   graph_map;   //
 
-    struct __Node{
-        weight_type       cost;
-        CROSS::id_type    cross_id;
 
-        __Node *       parent;
-		Node*          p_Node;
+
+	weight_type*                        p_weight;    // 每次计算最短路径之前，根据车速重定向该指针
+
+	struct __Node {
+		weight_type       cost;
+		CROSS::id_type    cross_id;
+
+		__Node *       parent;
+		Node *        p_Node;
 
 		__Node(weight_type _cost, CROSS::id_type _cross_id, __Node* _parent, Node* _p_Node) :
 			cost(_cost), cross_id(_cross_id), parent(_parent), p_Node(_p_Node) {}
-        ~__Node(){}
+		~__Node() { delete parent; }
 
-		struct Compare {};
-    };
+		struct Compare {
+			bool operator()(const __Node* a, const __Node* b) {
+				return a->cost > b->cost;
+			}
+		};
+	};
 
 };
-
-
-
+//----------------------
+//释放在行驶车辆占用的容量以及删除到达车辆
+void release_capacity(std::vector<CAR*>& car_running, int global_time, GRAPH *graph);
+//--------------------------
 #endif
