c972900182f3d91d07c4d330dc8c848c93beaa18
lebinlv <lvlebin@outlook.com>
2019-03-22 13:48:33 +0800

    modify add_node(), TODO: ~GRAPH(//free memory)

--- CodeCraft-2019/object.cpp
+++ CodeCraft-2019/object.cpp
@@ -1,22 +1,26 @@
 #include "lib/object.hpp"
 
-#include <queue> // 寻路算法中使用 priority_queue
-
 using namespace std;
 
 /********* class GRAPH  *********/
 
 GRAPH::idx_type GRAPH::Node::node_count = 0;
 
-void GRAPH::add_node(ROAD::id_type road_id, CROSS::id_type from, CROSS::id_type to,
+GRAPH::GRAPH(){p_weight = nullptr;}
+GRAPH::~GRAPH(){
+    // free weigth_map
+    // free graph_map
+}
+
+void GRAPH::add_node(ROAD::id_type road_id, CROSS::id_type from, CROSS::id_type to, ROAD::speed_type speed,
                      ROAD::capacity_type capacity, bool isDuplex)
 {
-    Node *p_Node = new Node(to, road_id);
+    Node *p_Node = new Node(to, road_id, speed);
     graph_map[from].push_back(p_Node);
     capacity_vec.push_back(capacity);
 
     if(isDuplex) {
-        p_Node = new Node(from, road_id);
+        p_Node = new Node(from, road_id, speed);
         graph_map[to].push_back(p_Node);
         capacity_vec.push_back(capacity);
     }
@@ -47,12 +51,12 @@ GRAPH::route_type & GRAPH::get_least_cost_route(CROSS::id_type from, CROSS::id_t
     __Node *mid;
     vector<__Node *> del_nodes;
     while (visited_map.find(to) == visited_map.end()) {
-        vector<Node *> &start = graph_map[pre_index];
+        vector<Node *> & start = graph_map[pre_index];
 
         for (int i = 0; i < start.size(); i++) {
             // 判断这个边的容量是否小于等于0, 如果这个边的容量大于0，建立__Node实例, 并把它压入优先队列
-            if() { 
-                weight_type cost_mid = distance[pre_index] + p_weight[start[i]->weight_idx];
+            if(i) { 
+                weight_type cost_mid = distance[pre_index] + p_weight[start[i]->info_idx];
                 CROSS::id_type cross_id_mid = start[i]->cross_id;
                 __Node *parent_mid = pre_node;
                 Node *p_Node_mid = start[i];
@@ -75,16 +79,14 @@ GRAPH::route_type & GRAPH::get_least_cost_route(CROSS::id_type from, CROSS::id_t
         pre_node = record;                         // 修改父节点信息
         pre_index = record->cross_id;
     }
-    auto answer = new vector<CROSS::id_type>; // answer这个vector存储的是从目的节点到源节点的边的地址的序列
+    auto answer = new vector<Node*>; // answer这个vector存储的是从目的节点到源节点的边的地址的序列
 
     while (record != nullptr) {
         answer->push_back(record->p_Node);    // 从目的节点开始存路径节点(Node)的地址
         record = record->parent;
     }
 
-    for (int i = 0; i < del_nodes.size(); i++) {
-        delete del_nodes[i];
-    }
+    for_each(del_nodes.begin(), del_nodes.end(), [](__Node *val) -> void {delete val;});
     //这边各个点容量减少一
 
     return *answer;
