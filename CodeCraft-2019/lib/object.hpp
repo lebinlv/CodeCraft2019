047cd05aa6604dc3b3bdd92cabd48b065cb60b6d
lebinlv <lvlebin@outlook.com>
2019-03-23 14:05:34 +0800

    添加GRAPH::Node::ROAD* pRoad以替代length, max_speed等变量

--- CodeCraft-2019/lib/object.hpp
+++ CodeCraft-2019/lib/object.hpp
@@ -7,50 +7,29 @@
 #include <queue>
 #include <algorithm>
 
-struct CROSS
-{
-    typedef uint8_t     id_type;  // 路口id类型
-};
 
 struct ROAD
 {
-    typedef uint16_t    id_type;
-    typedef uint8_t     length_type;
-    typedef uint8_t     speed_type;
-    typedef uint8_t     channel_type;
-    typedef int16_t     capacity_type;
-
     // (id,length,speed,channel,from,to,isDuplex)
-    id_type             id;
-    length_type         length;
-    speed_type          max_speed;
-    capacity_type       capacity;
-    channel_type        channel;
+    int id, length, max_speed, channel, from, to;
+    int ini_capacity;
+    bool isDuplex;
 
     ROAD(){}
-    ROAD(id_type _id, length_type _length, speed_type _speed, channel_type _channel):
-         id(_id), length(_length), max_speed(_speed), channel(_channel) {
-        capacity = _length * _channel;
-    }
+    ROAD(int _id, int _length, int _speed, int _channel, int _from, int _to, bool _isDuplex):
+         id(_id), length(_length), max_speed(_speed), channel(_channel),
+         from(_from), to(_to), isDuplex(_isDuplex) {ini_capacity = _length * _channel;}
     ~ROAD(){}
 };
 
 
 struct CAR
 {
-    typedef uint16_t    id_type;
-    typedef uint8_t     speed_type;
-    typedef uint8_t     time_type;
-
     // (id, from, to, speed, planTime)
-    id_type             id;
-    CROSS::id_type      from;
-    CROSS::id_type      to;
-    speed_type          speed;
-    time_type           plan_time;
+    int id, from, to, speed, plan_time;
 
     CAR() {}
-    CAR(id_type _id, CROSS::id_type _from, CROSS::id_type _to, speed_type _speed, time_type _time):
+    CAR(int _id, int _from, int _to, int _speed, int _time):
         id(_id), from(_from), to(_to), speed(_speed), plan_time(_time) {}
     ~CAR() {}
 
@@ -77,27 +56,16 @@ struct CAR
 class GRAPH
 {
   public:
-    typedef double                    weight_type;  // 边的权重的数据类型
-    typedef uint16_t                  idx_type;     // 下标的数据类型,根据边的数目确定
-
     struct Node{
-        CROSS::id_type       cross_id;        // 本节点代表的路口的id
-        ROAD::id_type        road_id;         // 到达本节点的路的id
-        ROAD::length_type    length;          // 到达本节点的路的长度
-        ROAD::speed_type     max_speed;       // 到达本节点的路的最大速度
-        ROAD::capacity_type  capacity;        // 到达本节点的边的容量
-        idx_type             info_idx;        // 权重和容量信息均通过info_idx访问
+        int                           cross_id;   // 本节点代表的路口的id
+        int                           capacity;   // 到达本节点的边的容量
+        int                           info_idx;   // 权重和容量信息均通过info_idx访问
+        ROAD *                        pRoad;      // 通过 pRoad 获取道路长度、最大速度、车道数、是否双向等信息
 
-        static idx_type node_count;           // 静态变量用于统计节点个数，初始值为0
-        static std::vector<Node *> pNode_vec; // 为了便于计算 weight_map 而添加的vector
+        static int                    node_count; // 静态变量用于统计节点个数，初始值为0
+        static std::vector<ROAD *>    pRoad_vec;  // 为了便于计算 weight_map 而添加的vector
 
-        Node(CROSS::id_type _cross_id, ROAD::id_type _road_id,
-             ROAD::length_type _length, ROAD::speed_type  _speed, ROAD::capacity_type  _capacity) : 
-             cross_id(_cross_id), road_id(_road_id),
-             length(_length), max_speed(_speed), capacity(_capacity){
-            info_idx = node_count++;
-            pNode_vec.push_back(this);
-        }
+        Node(int _cross_id, ROAD *_pRoad);
         ~Node(){}
     };
 
@@ -109,15 +77,10 @@ class GRAPH
     /**
      * @brief 根据道路信息添加节点
      * 
-     * @param road_id  道路id
-     * @param from     道路起点路口id
-     * @param to       道路终点路口id
-     * @param isDuplex 是否双向
-     * 
-     * @attention 此函数并不检查 road_id 是否重复
+     * @param pRoad 指向道路的指针
+     * @param cross_id 这条边的终点
      */
-    void add_node(ROAD::id_type road_id, CROSS::id_type from, CROSS::id_type to, ROAD::length_type length,
-                  ROAD::speed_type speed, ROAD::capacity_type capacity, bool isDuplex);
+    void add_node(ROAD* pRoad);
 
     /**
      * @brief 根据车速计算新的权重数组
@@ -125,9 +88,8 @@ class GRAPH
      * @param speed 车速
      * @attention   必须在添加所有节点之后调用该函数
      */
-    void add_weight_accord_to_speed(CAR::speed_type speed);
-
-    void update_weights(bool speed_detect_array[], int size);
+    void add_weights(int speed);
+    void add_weights(bool speed_detect_array[], int size);
 
     /**
      * @brief Get the least cost route object
@@ -137,25 +99,25 @@ class GRAPH
      * @param speed 
      * @return route_type& 返回 vector<Node *>，如果失败则vector为空，如果成功，逆序遍历此vector便可得到路径
      */
-    route_type & get_least_cost_route(CROSS::id_type from, CROSS::id_type to, CAR::speed_type speed);
+    route_type & get_least_cost_route(int from, int to, int speed);
 
 
   private:
-    std::map<CAR::speed_type, weight_type*>         weight_map;  // 边对于不同速度的车，具有不同的权重
-    std::map<CROSS::id_type, std::vector<Node*> >   graph_map;   //
+    std::map<int, double*>               weight_map;  // 边对于不同速度的车，具有不同的权重
+    std::map<int, std::vector<Node*> >   graph_map;   //
     //std::vector<ROAD::capacity_type>                capacity_vec;// 
 
 
-    weight_type*                        p_weight;    // 每次计算最短路径之前，根据车速重定向该指针
+    double*                        p_weight;    // 每次计算最短路径之前，根据车速重定向该指针
 
     struct __Node{
-        weight_type       cost;
-        CROSS::id_type    cross_id;
+        double       cost;
+        int          cross_id;
 
-        __Node *          parent;   // 用于到达终点时回溯得到路径
-        Node *            p_Node;
+        __Node *     parent;   // 用于到达终点时回溯得到路径
+        Node *       p_Node;
 
-        __Node(weight_type _cost, CROSS::id_type _cross_id, __Node* _parent, Node* _p_Node) :
+        __Node(int _cost, int _cross_id, __Node* _parent, Node* _p_Node) :
                cost(_cost), cross_id(_cross_id), parent(_parent), p_Node(_p_Node){}
         ~__Node(){}
 
